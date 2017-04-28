94d40733aa9129d72873fb69a8a29cadfe36f7a6
Samantha Whisenant <X00309798@wcc.dom>
2017-04-28 10:29:34 -0500

    Adding week 13 Date Printer Project

--- Date.py
+++ Date.py
@@ -1,12 +1,15 @@
 def main():
 
-   print('The new date is:', date_converter())
-
-
-
-def date_converter():
    print('Enter a date (mm/dd/yyyy): ', end='')
    date = str(input())
+
+   print('The new date is: ', end='')
+
+   print(date_converter(date))
+
+
+
+def date_converter(date):
 
    date_list = date.split('/')
 
@@ -17,7 +20,7 @@ def date_converter():
 
    month = list2[index]
 
-   new = print(month, " ", date_list[1], ",", date_list[2], sep='')
+   new = month + " " + date_list[1] + "," + date_list[2]
 
    return new
 
